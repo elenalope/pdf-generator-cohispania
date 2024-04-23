0e21067deb3c1305309b006ec599109c3d7a8ff5
Nho89 <noa1720@gmail.com>
2024-04-23 12:14:15 +0200

    feat(server): add app.js  and config.js

--- server/app.js
+++ server/app.js
@@ -3,10 +3,18 @@ import {PORT} from './config.js'
 import {connect} from './database/db.js'
 import express from 'express'
 import cors from 'cors'
+import pdfRouter from './routes/pdf.js'
 
 
 const app = express()
 
 app.use(cors());
 app.use(express.json());
-app.use('/', );
+app.use('/api', pdfRouter);
+
+
+connect();
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
