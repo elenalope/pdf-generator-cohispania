8ec6a75a767df093b8dea1d16de6939c19baab73
Nho89 <noa1720@gmail.com>
2024-04-24 15:32:55 +0200

    feat(router): add routes app

--- server/app.js
+++ server/app.js
@@ -3,15 +3,14 @@ import {PORT} from './config.js'
 import {connect} from './database/db.js'
 import express from 'express'
 import cors from 'cors'
-/* import pdfRouter from './routes/pdf.js'
- */import Template from './models/Template.js'
+import {pdfRouter} from './routes/pdfRouter.js'
 
 
 const app = express()
 
 app.use(cors());
 app.use(express.json());
-/* app.use('/api', pdfRouter); */
+app.use('/', pdfRouter)
 
 
 connect();
