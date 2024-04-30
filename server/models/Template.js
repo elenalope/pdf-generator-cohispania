7c20127744e00156ca722f92c91bda53be58513d
Nho89 <noa1720@gmail.com>
2024-04-30 09:44:53 +0200

    feat(models): add data in the models

--- server/models/Template.js
+++ server/models/Template.js
@@ -1,4 +1,4 @@
-import mongoose, { Schema } from 'mongoose';
+import mongoose from 'mongoose';
 
 const SubsectionSchema = new mongoose.Schema({
     title: String,
@@ -9,22 +9,27 @@ const SubsectionSchema = new mongoose.Schema({
 
 const SectionSchema = new mongoose.Schema({
     title: String,
+    paragraph: String,
+    signature: String,
     cover: Boolean,
     image: String,
     link: String,
-    paragraph: String,
+    break: Boolean,
+    list: [String],
     content: [String],
     Subsection: [SubsectionSchema]
 },{_id : false})
 
 const ChapterSchema = new mongoose.Schema({
     title: String,
+    subtitle: String,
+    image: String,
     content: [SectionSchema]
 },{_id : false});
 
 const TemplateSchema = new mongoose.Schema({
     title: String,
-    content: [ChapterSchema]
+    content: [ChapterSchema, SectionSchema]
 });
 
 const Template = mongoose.model('Template', TemplateSchema);
