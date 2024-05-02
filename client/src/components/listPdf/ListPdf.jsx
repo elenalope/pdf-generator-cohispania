2c74845dd58fe442c8a0988468b5cec2ecfe5847
Nho89 <noa1720@gmail.com>
2024-05-03 01:18:46 +0200

    feat(listPdf):modified reception data

--- client/src/components/listPdf/ListPdf.jsx
+++ client/src/components/listPdf/ListPdf.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import { getPDF } from '../../services/pdfServices';
+
 
 const ListPdf = () => {
+
+  const [documents, setDocuments] = useState([]);
+
+  useEffect(()=>{
+
+    const getDocuments = async () => {
+      const response = await getPDF();
+      console.log(response)
+       setDocuments(response)
+      }
+
+    getDocuments();
+  }, [])
+
+
   return (
-    <div className='listPdfContainer'>ListPdf</div>
-  )
-}
+    <div className='listPdfContainer'>
+      <h3>Lista de plantillas</h3>
+      <ul>
+        {documents.map((document) => (
+          <li key={document._id}>{document.title}</li>
+        ))}
+      </ul>
+     
+    </div>
+  );
+};
 
 export default ListPdf
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom';
 import router from './router/router.jsx';
+import DocumentProvider from './context/DocumentContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <RouterProvider router={router} />,
+    
+    <DocumentProvider>
+        <RouterProvider router={router} />
+    </DocumentProvider>
 )
