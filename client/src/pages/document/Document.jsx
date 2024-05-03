cc0eb83fdf8dd97db2b5c12551155415f3ef5f89
elenalope <elopmorales97@gmail.com>
2024-05-03 12:03:39 +0200

    feat(Document):styles document

--- client/src/pages/document/Document.jsx
+++ client/src/pages/document/Document.jsx
@@ -4,8 +4,10 @@ function Document() {
   return (
     <>
     <div className='template-name'>Nombre de la plantilla</div>
-    <div className='option-list'></div>
-    <div className='pdf-background'></div>
+    <div className='document-body'>
+      <div className='option-list'></div>
+      <div className='pdf-background'></div>
+    </div>
     <button className='exit-button'>SALIR SIN GUARDAR</button>
     </>
   )
