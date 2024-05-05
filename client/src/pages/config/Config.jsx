8ed41e5015623a98ffd857ec313d29d57a7e8abb
Nho89 <noa1720@gmail.com>
2024-05-06 00:50:23 +0200

    feat(client/config):modified style component form

--- client/src/pages/config/Config.jsx
+++ client/src/pages/config/Config.jsx
@@ -10,6 +10,7 @@ import Button from '@mui/material/Button';
 import SaveIcon from '@mui/icons-material/Save';
 import GetAppIcon from '@mui/icons-material/GetApp';
 import VisibilityIcon from '@mui/icons-material/Visibility';
+import FormControlLabel from '@mui/material/FormControlLabel';
 
 
 const Config = () => {
@@ -18,14 +19,23 @@ const Config = () => {
     const[title, setTitle] = useState('');
     const[subtitle, setSubtitle] = useState('');
     const[toc, setToc] = useState(false);
+    const[padding, setPadding] = useState('')
     const[coverLogo, setCoverLogo] = useState(null);
-    const [showPreview, setShowPreview] = useState(false);
+    const[showPreview, setShowPreview] = useState(false);
+    const[includeBackCover, setIncludeBackCover] = useState(false)
+    const[includeCover, setIncludeCover] = useState(false)
+    const[theme, setTheme] = useState('')
+    const[highlightedValue, setHighlightedValue] = useState('')
+    const[docExplanation, setDocExplanation] = useState('')
+    const[coverImg, setCoverImg] = useState('')
+    const[headerLogo, setHeaderLogo] = useState('')
+    const[watermark, setWatermark] = useState('')
     const navigate = useNavigate();
     
 
     const handleConfig = () => {
 
-        navigate('/document', {state: {config: {size, title, subtitle,coverLogo, toc, theme, padding, highlightedValue, docExplanation, coverImg, headerLogo, watermark, orientation }}});
+        navigate('/document', {state: {config: {size, title, subtitle,coverLogo, toc, theme, padding, highlightedValue, docExplanation, coverImg, headerLogo, watermark, includeCover,includeBackCover }}});
     }
     const handlePreview = () =>{
         setShowPreview(!showPreview);
@@ -62,12 +72,50 @@ const Config = () => {
             <TextField id='subtitle' label="Subtítulo" variant='standard' value={subtitle} onChange={(e)=> setSubtitle(e.target.value)}/>
         </label>
         <label className='configLabel'>
-                Incluir Índice:
-                <Switch id="toc-switch"  checked={toc} onChange={(e) => setToc(e.target.checked)} inputProps={{ 'aria-label': 'controlled' }}/>
+        <FormControlLabel
+            control={
+        <Switch id="toc-switch" checked={toc}
+            onChange={(e) => setToc(e.target.checked)}
+            inputProps={{ 'aria-label': 'controlled' }}
+        />
+            } label="Índice"/>
         </label>
         <label htmlFor="" className='configLabel'>
             <TextField id='coverLogo' label="cover Logo" variant='standard' value={coverLogo} onChange={(e)=> setCoverLogo(e.target.value)}/>
         </label>
+        <label id="demo-simple-select-filled-label" className='configLabel'>
+            Padding:
+        <Select
+          labelId="demo-simple-select-filled-label"
+          id="demo-simple-select-filled"
+          value={padding}
+          onChange={(e)=> setPadding(e.target.value)}
+          label="Padding"
+        >
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+        </label>
+        <label className='configLabel'>
+        <FormControlLabel
+            control={
+        <Switch id="includeCover-switch" checked={includeCover}
+            onChange={(e) => setIncludeCover(e.target.checked)}
+            inputProps={{ 'aria-label': 'controlled' }}
+        />
+            } label="Portada"/>
+        </label>
+        <label className='configLabel'>
+        <FormControlLabel
+            control={
+        <Switch id="includeBackCover-switch" checked={includeBackCover}
+            onChange={(e) => setIncludeBackCover(e.target.checked)}
+            inputProps={{ 'aria-label': 'controlled' }}
+        />
+            } label="Contraportada"/>
+        </label>
+        
         <div className='containerButtonsConfig'>
             <Button variant="contained" onClick={handleConfig}>Crear </Button>
             <Button variant="outlined" onClick={()=>navigate('/')}>Cancelar</Button>
