2ec657067c0119fee2f99c9c51382b8bf436d68f
Nho89 <noa1720@gmail.com>
2024-05-07 10:51:17 +0200

    feat(document): add button dowload pdf

--- client/src/pages/document/MyDocument.jsx
+++ client/src/pages/document/MyDocument.jsx
@@ -3,27 +3,50 @@ import {useLocation, useNavigate} from 'react-router-dom';
 import './MyDocument.css';
 import { pdf, Document, Page, Text, View, StyleSheet } from '@react-pdf/renderer';
 import { PDFViewer } from '@react-pdf/renderer';
+import SaveIcon from '@mui/icons-material/Save';
+import GetAppIcon from '@mui/icons-material/GetApp';
+import VisibilityIcon from '@mui/icons-material/Visibility';
 
-
-/* const PdfDoc = ({ config }) => (
+const PdfDoc = ({ config }) => (
   <Document>
-     <Page size={config.size} style={styles.page}>
-       <View style={styles.section}>
+     <Page size={config.size}>
+       <View >
          <Text>{config.title}</Text>
          <Text>{config.subtitle}</Text>
        </View>
      </Page>
   </Document>
- ); */
+ );
 const MyDocument = () => {
+  const[showPreview, setShowPreview] = useState(false);
   const location = useLocation();
   const config = location.state?.config;
   const navigate = useNavigate();
 
+  const handlePreview = () =>{
+    setShowPreview(!showPreview);
+}
+
+const handleDownloadPdf = async () => {
+  
+  const blob = await pdf(<PdfDoc config={config} />).toBlob();
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement('a');
+  link.href = url;
+  link.setAttribute('download', 'documento.pdf');
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+};
  const { title, /* subtitle,coverLogo, toc, theme, padding, highlightedValue, docExplanation, coverImg, headerLogo, watermark, includeCover,includeBackCover */} = config;
   return (
     <>
-    <div className='template-name'>Nombre de la plantilla:{title}</div>
+    <div /* className='template-name' */><p>Nombre de la plantilla:{title}</p>
+        <ul >
+            <li><SaveIcon/></li>
+            <li><GetAppIcon onClick={handleDownloadPdf}/></li>
+            <li onClick={handlePreview} ><VisibilityIcon/></li>
+        </ul></div>
     <div className='document-body'>
       <div className='option-list'></div>
       <div className='pdf-background'></div>
