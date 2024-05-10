d87a224787e7b9769bcb4500dcf2e73e696c10f1
FatimaCapilla <fatima.capilla.sancho@gmail.com>
2024-05-10 09:40:07 +0200

    feat(home): add button to create the template

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom';
 import router from './router/router.jsx';
 import DocumentProvider from './context/DocumentContext.jsx';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     
