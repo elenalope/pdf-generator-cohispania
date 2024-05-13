f829e35e884bead08d0786572d1dc40f227355b0
BeatrizMercado <bea.mercado.lop@gmail.com>
2024-05-13 09:56:15 +0200

    feat(views):Implemented menu options layout in section.jsx using Material-UI

--- client/src/views/Section.jsx
+++ client/src/views/Section.jsx
@@ -1,8 +1,143 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+import MoveDownIcon from '@mui/icons-material/MoveDown';
+import Button from '@mui/material/Button';
+import BookIcon from '@mui/icons-material/Book';
+import ImportContactsIcon from '@mui/icons-material/ImportContacts';
+import TitleIcon from '@mui/icons-material/Title';
+import FormatAlignJustifyIcon from '@mui/icons-material/FormatAlignJustify';
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
+import ImageIcon from '@mui/icons-material/Image';
+import LinkIcon from '@mui/icons-material/Link';
+import DrawIcon from '@mui/icons-material/Draw';
+import AddIcon from '@mui/icons-material/Add';
 
-function Section() {
+const Section = () => {
+  const [section, setSection] = useState([]);
+  const [breaks, setBreaks] = useState([]);
+
+  const addSection = () => {
+    setSection([...section, 'Section']);
+  };
+
+  const addBreak = () => {
+    setBreaks([...breaks, 'Break']);
+  };
+
   return (
-    <div>Section</div>
+    <>
+    <div className='document-body'>
+      <div className='option-list'>
+      <Box>
+      <nav aria-label="main mailbox folders">
+        <List>
+        <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <ImportContactsIcon />
+              </ListItemIcon>
+              <ListItemText primary="Capítulo"/> 
+              <AddIcon />
+            </ListItemButton>
+          </ListItem>
+          <Divider />
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <BookIcon />
+              </ListItemIcon>
+              <ListItemText primary="Sección"/>
+              <AddIcon />
+            </ListItemButton>
+          </ListItem>
+          <Divider />
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <TitleIcon />
+              </ListItemIcon>
+              <ListItemText primary="Título"/>
+              <AddIcon />
+            </ListItemButton>
+          </ListItem>
+          <Divider />
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <FormatAlignJustifyIcon />
+              </ListItemIcon>
+              <ListItemText primary="Párrafo"/>
+              <AddIcon />
+            </ListItemButton>
+          </ListItem>
+          <Divider />
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <FormatListBulletedIcon />
+              </ListItemIcon>
+              <ListItemText primary="Lista"/>
+              <AddIcon />
+            </ListItemButton>
+          </ListItem>
+          <Divider />
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <DrawIcon />
+              </ListItemIcon>
+              <ListItemText primary="Firma"/>
+              <AddIcon />
+            </ListItemButton>
+          </ListItem>
+          <Divider />
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <ImageIcon />
+              </ListItemIcon>
+              <ListItemText primary="Imagen"/>
+              <AddIcon />
+            </ListItemButton>
+          </ListItem>
+          <Divider />
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <LinkIcon />
+              </ListItemIcon>
+              <ListItemText primary="Link"/>
+              <AddIcon />
+            </ListItemButton>
+          </ListItem>
+          <Divider />
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <MoveDownIcon />
+              </ListItemIcon>
+              <ListItemText primary="Salto"/>
+              <AddIcon />
+            </ListItemButton>
+          </ListItem>
+          <Divider />
+        </List>
+      </nav>
+    </Box>
+      </div>
+
+      <div className='pdf-background'>
+        
+      </div>
+    </div>
+    <Button variant="contained" sx={{ m:3 }}>SALIR SIN GUARDAR</Button>
+    </>
   )
 }
 
