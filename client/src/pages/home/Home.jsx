344850a7597ef80190be268f1b03eb9645a181cb
elenalope <elopmorales97@gmail.com>
2024-05-13 08:45:02 +0200

    feat(alerts):creating save success alert

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,15 +1,26 @@
-import './Home.css'
-import ListPdf from '../../components/listPdf/ListPdf'
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import AlertaDescargaExitosa from '../../components/alerts/SaveSuccessAlert';
+import ListPdf from '../../components/listPdf/ListPdf';
+import './Home.css';
 
 const Home = () => {
+    const [mostrarAlerta, setMostrarAlerta] = useState(false);
+
+    const handleMostrarAlerta = () => {
+        setMostrarAlerta(true);
+    };
+
     return (
-      <>
-      <h3 className="boxHome">Mis plantillas</h3>
-          <ListPdf/>
-           
-      </>
-          
+        <>
+            <h3 className="boxHome">Mis plantillas</h3>
+            <ListPdf />
+            <Button variant="contained" onClick={handleMostrarAlerta}>
+                Mostrar Alerta
+            </Button>
+            {mostrarAlerta && <AlertaDescargaExitosa onClose={() => setMostrarAlerta(false)} />}
+        </>
     );
-  }
-  
-  export default Home;
+};
+
+export default Home;
