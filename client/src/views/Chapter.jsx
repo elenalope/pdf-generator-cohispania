273f53b7711626348d661f51f2f389e7bfc7cfee
rebecavm28 <rebe14@hotmail.es>
2024-05-14 11:30:31 +0200

    feact:chapter-material ui

--- client/src/views/Chapter.jsx
+++ client/src/views/Chapter.jsx
@@ -1,11 +1,35 @@
 import React, { useState } from 'react';
 import AddIcon from '@mui/icons-material/Add';
-import Button from '@mui/material/Button'; 
-import MyDocument from "../pages/document/MyDocument.jsx";
-import { useLocation, useNavigate } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import { useLocation, useNavigate, Link } from 'react-router-dom';
 import axios from 'axios';
 import ImportContactsIcon from '@mui/icons-material/ImportContacts';
-import './Chapter.css'; 
+import './Chapter.css';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+import MoveDownIcon from '@mui/icons-material/MoveDown';
+import BookIcon from '@mui/icons-material/Book';
+import TitleIcon from '@mui/icons-material/Title';
+import FormatAlignJustifyIcon from '@mui/icons-material/FormatAlignJustify';
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
+import ImageIcon from '@mui/icons-material/Image';
+import LinkIcon from '@mui/icons-material/Link';
+import DrawIcon from '@mui/icons-material/Draw';
+import Switch from '@mui/material/Switch';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormGroup from '@mui/material/FormGroup';
+import SendIcon from '@mui/icons-material/Send';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import { styled } from '@mui/material/styles';
+import TextField from '@mui/material/TextField';
+
+
+
 
 const Chapter = (data) => {
   const [formDataArray, setFormDataArray] = useState([]);
@@ -43,43 +67,163 @@ const Chapter = (data) => {
   }
 
   return (
-    <div>
-      <MyDocument/> 
       <div>
-        <button className="Chapter">Capitulo</button>
         <div className='add'>
-          <Button onClick={handleClick}>
+          <Button >
             <AddIcon/>
           </Button>
           <ImportContactsIcon className='book'/>
+          <div className='document-body'>
+      <div className='option-list'>
+      <Box>
+      <nav>
+        <List sx={{ marginTop: '190%',right:'65%', width:'75%' ,backgroundColor: '#E9EAEC'}}>
+  <ListItem disablePadding>
+    <ListItemButton>
+      <ListItemIcon>
+        <ImportContactsIcon />
+      </ListItemIcon>
+      <ListItemText primary="Capítulo" />
+      <AddIcon onClick={handleClick}/>
+    </ListItemButton>
+  </ListItem>
+  <Divider />
+  <ListItem disablePadding>
+    <ListItemButton disabled>
+      <ListItemIcon>
+        <BookIcon />
+      </ListItemIcon>
+      <ListItemText primary="Sección" />
+      <AddIcon />
+    </ListItemButton>
+  </ListItem>
+  <Divider />
+  <ListItem disablePadding>
+    <ListItemButton disabled>
+      <ListItemIcon>
+        <TitleIcon />
+      </ListItemIcon>
+      <ListItemText primary="Título" />
+      <AddIcon />
+    </ListItemButton>
+  </ListItem>
+  <Divider />
+  <ListItem disablePadding>
+    <ListItemButton disabled>
+      <ListItemIcon>
+        <FormatAlignJustifyIcon />
+      </ListItemIcon>
+      <ListItemText primary="Párrafo" />
+      <AddIcon />
+    </ListItemButton>
+  </ListItem>
+  <Divider />
+  <ListItem disablePadding>
+    <ListItemButton disabled>
+      <ListItemIcon>
+        <FormatListBulletedIcon />
+      </ListItemIcon>
+      <ListItemText primary="Lista" />
+      <AddIcon />
+    </ListItemButton>
+  </ListItem>
+  <Divider />
+  <ListItem disablePadding>
+    <ListItemButton disabled>
+      <ListItemIcon>
+        <DrawIcon />
+      </ListItemIcon>
+      <ListItemText primary="Firma" />
+      <AddIcon />
+    </ListItemButton>
+  </ListItem>
+  <Divider />
+  <ListItem disablePadding>
+    <ListItemButton disabled>
+      <ListItemIcon>
+        <ImageIcon />
+      </ListItemIcon>
+      <ListItemText primary="Imagen" />
+      <AddIcon />
+    </ListItemButton>
+  </ListItem>
+  <Divider />
+  <ListItem disablePadding>
+    <ListItemButton disabled>
+      <ListItemIcon>
+        <LinkIcon />
+      </ListItemIcon>
+      <ListItemText primary="Link" />
+      <AddIcon />
+    </ListItemButton>
+  </ListItem>
+  <Divider />
+  <ListItem disablePadding>
+    <ListItemButton disabled>
+      <ListItemIcon>
+        <MoveDownIcon />
+      </ListItemIcon>
+      <ListItemText primary="Salto" />
+      <AddIcon />
+    </ListItemButton>
+     </ListItem>
+     </List>
+
+      </nav>
+    </Box>
+      </div>
         </div>
       </div>
       {formDataArray.map((formData, index) => (
         <form key={index} className="form" onSubmit={(e) => handleSubmit(e, index)}>
-          <input 
-            type="text"
-            name="title"
-            placeholder="Título"
-            value={formData.title}
-            onChange={(e) => handleInputChange(e, index)}
-          />
-          <input 
-            type="text"
-            name="subtitle"
-            placeholder="Subtítulo"
-            value={formData.subtitle}
-            onChange={(e) => handleInputChange(e, index)}
-          />
-          <input 
-            type="file"
-            name="image"
-            onChange={(e) => handleInputChange(e, index)}
-          />
-          <button type="submit">Guardar</button>
+          <div className='container-target'style={{ marginTop: 'px' }}>
+          <TextField
+              sx={{ mb: 2}}
+              label="Título"
+              type="text"
+              variant="standard"
+              value={formData.title}
+              onChange={(e) => handleInputChange(e, index)}
+                  />
+               <Button
+                sx={{ mb: 2 }}
+                component="label"
+                role={undefined}
+                variant="contained"
+                tabIndex={-1}
+                startIcon={<CloudUploadIcon />}
+                   >
+                 Seleccionar Imagen
+                 <VisuallyHiddenInput type="file" onChange={(e) => handleInputChange(e, index)} />
+                 </Button>
+
+                <FormControlLabel control={<Switch />} label="Portada" 
+                value={formData.cover}
+                onChange={(e) => handleInputChange(e, index)}/>
+
+                <Button variant="contained" endIcon={<SendIcon />} size="small"
+                sx={{ width: 100 , ml: 'auto'}} type="submit" onSubmit={handleSubmit()}>
+                Crear
+                </Button>
+              <Box/>
+              </div>
         </form>
       ))}
     </div>
   );
 }
 
+
+
+
+
+
+
 export default Chapter;
+
+
+
+
+
+
+
