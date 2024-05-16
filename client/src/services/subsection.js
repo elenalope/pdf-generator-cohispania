9f45022a2dda686135440378fbbb1a9481d96bc4
rebecavm28 <rebe14@hotmail.es>
2024-05-16 12:09:54 +0200

    feact:subsection-services

---
+++ client/src/services/subsection.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+export const URL = 'http://localhost:3000/api/document';
+
+
+export const addsubSection = async (templateId, newsubSection) => {
+    try {
+      console.log("Datos del subSection enviados:", sectionData);
+      const response = await axios.post(`${URL}/${templateId}/subsection`, newsubSection);
+      alert("subSección añadidaa exitosamente");
+      return response;
+    } catch (error) {
+       console.error('Error creating subsection', error.message);
+       throw error; }
+   };
+
+export const updateSection = async (templateId, subsectionId, updatedsubSection)=>{
+  try {
+    const response = await axios.put(`${URL}/${templateId}/subsection/${subsectionId}`, {subsection: updatedSection});
+    alert("subSección actualizada exitosamente");
+    return response;
+  } catch (error) {
+    console.error('Error updating subsection', error.message);
+    throw error;
+  }
+};
+
+export const deletesubSection = async (templateId, subSectionId)=>{
+  try {
+    const response = await axios.delete(`${URL}/${templateId}/subSection/${subSectionIdsectionId}`);
+    alert('subSección eliminada correctamente');
+    return response;
+  } catch (error) {
+    console.error('Error deleting subsection', error.message);
+    throw error;
+  }
+}
