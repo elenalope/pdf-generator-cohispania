4ebf75d7bc199fcf28fbf665c69ea422ba6bfdd0
elenalope <elopmorales97@gmail.com>
2024-05-20 10:20:19 +0200

    feat(createtemplate):create template alert

---
+++ client/src/components/alerts/CreateTemplate.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import './Alerts.css';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import Button from '@mui/material/Button';
+import { CheckCircleOutline } from '@mui/icons-material';
+import { useNavigate } from 'react-router-dom';
+
+const CreateTemplate = ({ onClose }) => {
+    const [open, setOpen] = useState(true);
+    const navigate = useNavigate();
+
+    const handleClose = () => {
+        setOpen(false);
+        onClose && onClose();
+    };
+
+    return (
+        <Dialog open={open} onClose={handleClose} maxWidth="xs" fullWidth >
+            <DialogContent sx={{ padding: '0', backgroundColor: 'transparent' }}>
+                <div style={{ backgroundColor: '#12C069', textAlign: 'center', padding: '50px 0'}}>
+                    <CheckCircleOutline sx={{ fontSize: '70px', color: 'white' }} />
+                </div>
+            </DialogContent>
+            <DialogContent sx={{ backgroundColor: 'white', textAlign: 'center', padding: '10px' }}>
+                <h2 style={{ fontFamily: 'Open Sans', fontWeight: 'bold' }}>¡Enhorabuena, estás comenzando a crear tu PDF!</h2>
+                <h3 style={{ fontFamily: 'Open Sans', fontWeight: '300' }}>Se ha guardado con éxito</h3>
+            </DialogContent>
+            <DialogContent sx={{ textAlign: 'center', padding: '10px' }}>
+                <Button variant="contained" onClick={handleClose} size="large">
+                    OK
+                </Button>
+            </DialogContent>
+        </Dialog>
+    )
+};
+
+export default CreateTemplate;
